e47f131dc47e48ca1ef8adb4bbb6ee39a75293e7
agyzfh <36189236+agyzfh@users.noreply.github.com>
2019-10-05 13:36:20 +0800

    Create Affinity_propagation_digit.py

---
+++ First_Homework/Affinity_propagation_digit.py
@@ -0,0 +1,50 @@
+from sklearn.cluster import AffinityPropagation
+from sklearn import metrics
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.datasets import load_digits
+from sklearn.preprocessing import scale
+import numpy as np
+from sklearn.decomposition import PCA
+# #############################################################################
+# Generate sample data
+#centers = [[1, 1], [-1, -1], [1, -1]]
+
+digits = load_digits()
+data = scale(digits.data)
+n_samples, n_features = data.shape
+n_digits = len(np.unique(digits.target))
+labels_true = digits.target
+# Compute Affinity Propagation
+af = AffinityPropagation(preference=-5000).fit(data)
+cluster_centers_indices = af.cluster_centers_indices_
+labels = af.labels_
+
+n_clusters_ = len(cluster_centers_indices)
+
+print('Estimated number of clusters: %d' % n_clusters_)
+
+print("Adjusted Mutual Information: %0.3f" % metrics.adjusted_mutual_info_score(labels_true, labels,average_method='arithmetic'))
+print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+
+# Plot result
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+plt.close('all')
+plt.figure(1)
+plt.clf()
+
+colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+for k, col in zip(range(n_clusters_), colors):
+    class_members = labels == k
+
+    cluster_center = data[cluster_centers_indices[k]]
+    data = PCA(n_components=2).fit_transform(data)
+    plt.plot(data[class_members, 0], data[class_members, 1], col + '.')
+    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,markeredgecolor='k', markersize=14)
+    for x in data[class_members]:
+        plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], col)
+
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
