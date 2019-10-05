770cae0aaba9d836748cd19cfecea5cd6360b8e3
agyzfh <36189236+agyzfh@users.noreply.github.com>
2019-10-05 14:05:07 +0800

    Create DBSCAN_digit.py

---
+++ First_Homework/DBSCAN_digit.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_digits
+from sklearn.preprocessing import scale
+import numpy as np
+from sklearn.decomposition import PCA
+
+# #############################################################################
+
+digits = load_digits()
+X = scale(digits.data)
+labels_true = digits.target
+# #############################################################################
+# Compute DBSCAN
+db = DBSCAN(eps=5, min_samples=15).fit(X)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+
+# Number of clusters in labels, ignoring noise if present.
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
+
+print('Estimated number of clusters: %d' % n_clusters_)
+print('Estimated number of noise points: %d' % n_noise_)
+print("Adjusted Mutual Information: %0.3f" % metrics.adjusted_mutual_info_score(labels_true, labels,average_method='arithmetic'))
+print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+
+# #############################################################################
+# Plot result
+import matplotlib.pyplot as plt
+
+# Black removed and is used for noise instead.
+unique_labels = set(labels)
+colors = [plt.cm.Spectral(each)
+          for each in np.linspace(0, 1, len(unique_labels))]
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = [0, 0, 0, 1]
+
+    class_member_mask = (labels == k)
+    X = PCA(n_components=2).fit_transform(X)
+    xy = X[class_member_mask & core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=7)
+
+    xy = X[class_member_mask & ~core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=3)
+
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
